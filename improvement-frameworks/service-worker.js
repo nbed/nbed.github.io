e973deaf6ec1d6a6cc3535bf6d2da19e7f43bc3d
Brent Simmons <brent.simmons@unb.ca>
2019-10-14 15:21:37 -0300

    Updating hyperlink

--- improvement-frameworks/service-worker.js
+++ improvement-frameworks/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","8dc48300b51f8562f0806dd0b936cf7f"],["src/main-element.js","578cbb60914b3c5bc2d584c05b4f4ccb"]];
+var precacheConfig = [["index.html","8dc48300b51f8562f0806dd0b936cf7f"],["src/main-element.js","a38e66cbe6aa78a496d0baf49009596c"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
