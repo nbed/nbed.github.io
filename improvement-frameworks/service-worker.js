e4eff5981dd4e12df3d5f1a51e33250a9cf8dc81
Brent2 <brents@patriot1tech.com>
2021-08-25 09:07:37 -0300

    Updating production

--- improvement-frameworks/service-worker.js
+++ improvement-frameworks/service-worker.js
@@ -37,7 +37,7 @@
 /* eslint-disable indent, no-unused-vars, no-multiple-empty-lines, max-nested-callbacks, space-before-function-paren, quotes, comma-spacing */
 'use strict';
 
-var precacheConfig = [["index.html","8dc48300b51f8562f0806dd0b936cf7f"],["src/main-element.js","0b67002e67b93412e2d8dc7007324f0c"]];
+var precacheConfig = [["index.html","8dc48300b51f8562f0806dd0b936cf7f"],["src/main-element.js","d281d2b7497f62775c5c95c6a355d310"]];
 var cacheName = 'sw-precache-v3--' + (self.registration ? self.registration.scope : '');
 
 
